9e2bf611bdddef98539f818702f52aa73bd2816e
pwliu <skyliou520@gmail.com>
2017-04-23 15:19:45 +0800

    delete day3

--- day3/func_test6.py
+++
@@ -1,9 +0,0 @@
-__author__ = "Alex Li"
-
-def conn(host,port=3306):
-    pass
-
-
-
-
-#默认参数特点：调用函数的时候，默认参数非必须传递
--- day3/高阶函数.py
+++
@@ -1,9 +0,0 @@
-__author__ = "Alex Li"
-
-def add(a,b,f):
-    return f(a)+f(b)
-
-res = add(3,-6,abs)
-print(res)
-
-
