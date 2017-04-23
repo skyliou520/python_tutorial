afa893fbed542bd0b64aeb7f8e2e30e68d6c971e
pwliu <skyliou520@gmail.com>
2017-04-23 14:53:58 +0800

    init commit

---
+++ day3/func_test1.py
@@ -0,0 +1,21 @@
+__author__ = "Mark Liu"
+#函数
+def func1():
+    """testing1"""
+    print('in the func1')
+    return 0
+#过程
+def func2():
+    '''testing2'''
+    print('in the func2')
+
+x=func1()
+y=func2()
+
+print('from func1 return is %s' %x)
+print('from func2 return is %s' %y)
+
+# 函數有返回值
+# 過程沒有返回值
+
+
